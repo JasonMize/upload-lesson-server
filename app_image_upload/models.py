cacd28ed008dbd60b7874ad762a39f1990af9bac
Jason Mize <jasonemize@gmail.com>
2024-07-18 12:12:54 -0400

    updates

--- app_image_upload/models.py
+++ app_image_upload/models.py
@@ -10,4 +10,11 @@ class Profile(models.Model):
     return self.user.username
   
 
+class Image(models.Model):
+  title = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  image =  models.ImageField(upload_to='images/')
 
+  def __str__(self):
+    return self.title
+  
